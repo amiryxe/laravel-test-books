c017508e06a9185e8565efc5cedd2b88c514e5f5
Amir <nik.amirsa@gmail.com>
2020-07-16 22:52:48 +0430

    books list view

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,17 +3,19 @@
 @section('title') Books list @endsection
 
 @section('content')
-    @unless($books->isEmpty())
-        @foreach($books as $book)
-            <li>
-                <a href={{ '/books/' . $book->id }}>
-                    <h2>{{ $book->name }}</h2>
-                </a>
-                <h4>{{ $book->price }}</h4>
-            </li>
-        @endforeach
+    <ul class="books-list">
+        @unless($books->isEmpty())
+            @foreach($books as $book)
+                <li>
+                    <a href={{ '/books/' . $book->id }}>
+                        <h2>{{ $book->name }}</h2>
+                    </a>
+                    <h4>{{ $book->price }}</h4>
+                </li>
+            @endforeach
 
-    @else
-        <h2>No Books available!</h2>
-    @endif
+        @else
+            <h2>No Books available!</h2>
+        @endif
+    </ul>
 @endsection
