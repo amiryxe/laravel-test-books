be6166d67bda9a0212b9c5ab6f2b2fe639f23b3d
Amir <nik.amirsa@gmail.com>
2020-07-17 13:03:21 +0430

    products index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,5 +3,19 @@
 @section('title') Products List @endsection
 
 @section('content')
- prd list
+    <ul class="books-list">
+        @unless($products->isEmpty())
+            @foreach($products as $product)
+                <li>
+                    <a href={{ '/books/' . $product->id }}>
+                        <h2>{{ $product->name }}</h2>
+                    </a>
+                    <h4>{{ $product->price }}</h4>
+                </li>
+            @endforeach
+
+        @else
+            <h3>No Products available!</h3>
+        @endif
+    </ul>
 @endsection
