2d4ddab885af21ab42d09057281adde78f5a1943
Amir <nik.amirsa@gmail.com>
2020-07-19 23:58:57 +0430

    translate error message

--- resources/lang/fa/auth.php
+++ resources/lang/fa/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'مقدار وارد شده با اطلاعات ما منطبق نیست.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
