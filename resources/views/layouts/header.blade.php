7eea4bb6afcd38f3356f728346f5c64d8b31b5ef
amir <nik.amirsa@gmail.com>
2020-07-23 23:31:31 +0430

    add category item in navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,6 +16,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/products">Products</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/products/categories">Product categories</a>
+                </li>
             </ul>
         </div>
 
