c9ebb5384d5f4b88ac2ea1732b00aa38d025073d
amir <nik.amirsa@gmail.com>
2020-07-26 11:16:38 +0430

    create controller and migrations for shop

---
+++ database/migrations/2020_07_26_111508_create_product_shop_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductShopTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_shop', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_shop');
+    }
+}
